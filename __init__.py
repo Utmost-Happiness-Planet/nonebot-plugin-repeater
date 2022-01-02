9cbac7e3971611099d9973c3238fbe7401b8e1ea
Kl1nge5 <2351859943@qq.com>
2022-01-02 15:59:29 +0800

    🐛Fix bug | 📕 Add commits

--- __init__.py
+++ __init__.py
@@ -11,6 +11,7 @@ last_message = {}
 has_repeated = {}
 
 
+# 消息类型判别 - 分类普通文本消息、QQ表情、图片
 def messageType(message: str):
     if message[0] == "[":
         data = message.split(",")
@@ -24,8 +25,8 @@ async def repeater(bot: Bot, event: GroupMessageEvent, state: T_State):
     global last_message, has_repeated
     gid = str(event.group_id)
     if gid in repeater_group:
-        logger.debug(last_message)
         mt = messageType(str(event.message))
+        # 对不同类别的消息处理方式不同，图片是最麻烦的
         if mt == "Normal" and event.message == last_message.get(gid) and len(str(event.message)) >= shortest:
             data = event.message
         elif mt == "CQ:face" and event.message == last_message.get(gid):
@@ -34,15 +35,14 @@ async def repeater(bot: Bot, event: GroupMessageEvent, state: T_State):
             data = MessageSegment.image(str(event.message).split(",")[3][4:-1])
         else:
             data = None
+            has_repeated[gid] = False
 
         if mt == "CQ:image":
             last_message[gid] = str(event.message).split(",")[1]
         else:
             last_message[gid] = event.message
 
-        # logger.debug(data)
+        # 如果这条消息已经复读过了就不参与复读了
         if not has_repeated.get(gid) and data is not None:
             has_repeated[gid] = True
             await bot.send(event, data)
-        else:
-            has_repeated[gid] = False
