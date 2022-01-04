9b04861a2e310261d31aad948667e326952093d3
Kl1nge5 <2351859943@qq.com>
2022-01-04 13:11:01 +0800

    🔀 调整目录结构

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open("../README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="nonebot-plugin-repeater",
+    version="1.0.0",
+    author="Kl1nge5",
+    description="A plugin based on NoneBot2, auto +1 in group.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ninthseason/nonebot-plugin-repeater",
+    project_urls={
+        "Bug Tracker": "https://github.com/ninthseason/nonebot-plugin-repeater/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    packages=["nonebot_plugin_repeater"],
+    python_requires=">=3.7",
+)
