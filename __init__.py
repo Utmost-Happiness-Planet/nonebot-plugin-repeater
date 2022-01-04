31a33f32c733bde33bdbf9fc09e2cc3c2f98fa04
Kl1nge5 <2351859943@qq.com>
2022-01-04 12:31:40 +0800

    🐛Fix Bug

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from nonebot import get_driver, on_message, logger
 from nonebot.adapters.cqhttp import Bot, GroupMessageEvent, MessageSegment
 from nonebot.typing import T_State
+import re
 
 repeater_group = get_driver().config.repeater_group
 shortest = get_driver().config.repeater_minlen
@@ -20,6 +21,11 @@ def messageType(message: str):
         return "Normal"
 
 
+# 获取图片消息的url与hash
+def getPicMeta(message: str):
+    return re.findall("url=(.*?)[,|\]]", message)[0], re.findall("file=(.*?)[,|\]]", message)[0]
+
+
 @m.handle()
 async def repeater(bot: Bot, event: GroupMessageEvent, state: T_State):
     global last_message, has_repeated
@@ -28,21 +34,31 @@ async def repeater(bot: Bot, event: GroupMessageEvent, state: T_State):
         logger.debug(event.message)
         mt = messageType(str(event.message))
         logger.debug(mt)
-        # 对不同类别的消息处理方式不同，图片是最麻烦的
-        if mt == "Normal" and event.message == last_message.get(gid) and len(str(event.message)) >= shortest:
-            data = event.message
-        elif mt == "CQ:face" and event.message == last_message.get(gid):
-            data = event.message
-        elif mt == "CQ:image" and str(event.message).split(",")[1] == last_message.get(gid):
-            data = MessageSegment.image(str(event.message).split(",")[3][4:-1])
-        else:
-            data = None
-            has_repeated[gid] = False
 
-        if mt == "CQ:image":
-            last_message[gid] = str(event.message).split(",")[1]
-        else:
+        # 对不同类别的消息处理方式不同，图片是最麻烦的
+        data = None
+        if mt == "Normal":
+            if event.message == last_message.get(gid) and len(str(event.message)) >= shortest:
+                data = event.message
+            else:
+                has_repeated[gid] = False
             last_message[gid] = event.message
+        elif mt == "CQ:face":
+            if event.message == last_message.get(gid):
+                data = event.message
+            else:
+                has_repeated[gid] = False
+            last_message[gid] = event.message
+        elif mt == "CQ:image":
+            meta = getPicMeta(str(event.message))  # 图片消息元数据
+            if meta[1] == last_message.get(gid):
+                data = MessageSegment.image(meta[0])
+            else:
+                has_repeated[gid] = False
+            last_message[gid] = meta[1]
+        else:
+            last_message[gid] = None
+
         logger.debug(str(data))
         # 如果这条消息已经复读过了就不参与复读了
         if not has_repeated.get(gid) and data is not None:
