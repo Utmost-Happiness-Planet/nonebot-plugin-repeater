eac9b416d93ac826d311ca5c5b4b069b68935983
Kl1nge5 <2351859943@qq.com>
2022-04-27 16:07:26 +0800

    Fix a config bug

--- nonebot_plugin_repeater/config.py
+++ nonebot_plugin_repeater/config.py
@@ -5,10 +5,10 @@ config = get_driver().config.dict()
 if 'repeater_group' not in config:
     logger.warning('[复读姬] 未发现配置项 `repeater_group` , 采用默认值: []')
 if 'repeater_min_message_length' not in config:
-    logger.warning('[复读姬] 未发现配置项 `repeater_min_message_length` , 采用默认值: []')
+    logger.warning('[复读姬] 未发现配置项 `repeater_min_message_length` , 采用默认值: 1')
 if 'repeater_min_message_times' not in config:
-    logger.warning('[复读姬] 未发现配置项 `repeater_min_message_times` , 采用默认值: []')
+    logger.warning('[复读姬] 未发现配置项 `repeater_min_message_times` , 采用默认值: 2')
 
 repeater_group = config.get('repeater_group', [])
-shortest_length = config.get('repeater_min_message_length', [])
-shortest_times = config.get('repeater_min_message_times', [])
+shortest_length = config.get('repeater_min_message_length', 1)
+shortest_times = config.get('repeater_min_message_times', 2)
